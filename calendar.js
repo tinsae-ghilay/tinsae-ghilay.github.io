c018bd1b4daf282408507eb0fd9c2f21596efcd2
tinsae-ghilay <85639930+tinsae-ghilay@users.noreply.github.com>
2022-05-22 11:17:23 +0200

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -15,7 +15,7 @@ class Calendar{
         start=start.plusDays(-firstDay);// back to day one
         //console.log("start date is "+start.format());
         //start=start.plusDays(+start.dayOfWeek());
-        document.getElementById("title").innerHTML=displayedMonth+", "+thisYear;
+        document.getElementById("title").innerHTML=displayedMonth+", <span class=\"latin\">"+thisYear+"</span>";
         var bar=document.getElementById("calendarNav");
         bar.style.background="linear-gradient(to bottom, rgba(0,0,0,0) 50%, rgba(0,0,0,1)),url('./res/"+thisMonth+".webp')";
         bar.style.backgroundPosition="center";
@@ -23,8 +23,11 @@ class Calendar{
         //html+="<h3 style=\"align-text: center; width=\"100%\"\">"+displayedMonth+"</h3>";
         html+="<table>";
         html+="<tr style=\"padding:3px;\">";
-        html+="<th>"+weekDays[0]+"</th><th>"+weekDays[1]+"</th><th>"+weekDays[2]+"</th><th>"+weekDays[3]+"</th><th>"+weekDays[4]+"</th><th>"+weekDays[5]+"</th><th>"+weekDays[6]+"</th></tr></table>";
-        html+="<div class=\"month\">"
+        html+="<th class=\"weekend\">"+weekDays[0]+"</th><th>"+weekDays[1]+"</th><th>"+
+            weekDays[2]+"</th><th>"+weekDays[3]+"</th><th>"+weekDays[4]+"</th><th>"+
+            weekDays[5]+"</th><th class=\"weekend\">"+
+        weekDays[6]+"</th></tr></table>";
+        html+="<div class=\"month\" id=\"mon\">";
         for(let i=1;i<43;i++){
             if(start.month==thisMonth){
                 if(start.dayOfMonth==currentDate.dayOfMonth 
